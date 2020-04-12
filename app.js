6f267f426e764e764e4c210badb2cbf1dbde76fd
furkancesur <frkncesur@gmail.com>
2020-04-12 15:23:21 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require("express"),
+    app = express();
+
+app.set("view engine", "ejs");
+app.get("/", function (req,res) {
+    res.render("home");
+});
+app.get("/about", function (req, res) {
+    res.render("about");
+});
+
+app.listen(3000, function () {
+    console.log("Server listening on port 3000");
+});
